3347849b95d0f4d47bbe5682fa013d01afea04d6
AbouCloud <Abouzari6839@gmail.com>
2025-08-24 23:02:05 +0000

    form for add product and display on the page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,61 +1,95 @@
 import Navbar from "./component/Navbar"
 import filter from '../FiltersGetting'
 import './style/home.css'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 function Home() {
-
-    const query = [];
-    const [input, Setinput] = useState([])
-    const handleInput = (e) =>{
+    const [products, SetProducts] = useState([]);
+    const [query, Setquery] = useState({
+        Category: [],
+        Price: [],
+        Color: []
+    })
+    const queryString = new URLSearchParams(query).toString();
+    const handleQuery = (e, type) => {
         let valueItem = e.target.value;
-        let valueCheck = e.target.checked 
-            Setinput((prev) => {
-                const NewValue = valueCheck ? [...prev, valueItem] : prev.filter(ele => ele !== valueItem)
-                return NewValue;
-            }) 
+        let valueCheck = e.target.checked
+        Setquery((prev) => {
+            const NewValue = valueCheck ? [...prev[type], valueItem] : prev[type].filter(ele => ele !== valueItem)
+            return { ...prev, [type]: NewValue };
+        })
     }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const resp = await fetch(`http://localhost:5330/api/`);
+                if (!resp.ok) {
+                    throw new Error(`Please Check : ${resp.status}`)
+                }
+                const data = await resp.json();
+                SetProducts(data);
+            } catch (Err) {
+                console.log("Fetching Data Failed", Err)
+            }
+        }
+        fetchData();
+    }, [query])
 
     const filtersByCategory = filter.Category.map((item, index) => (
         <div className="" key={index}>
-            <input type="checkbox" onChange={handleInput} id={item.toLowerCase()} value={item} /> <label className="text-[15px] font-[lora]">{item}</label>
+            <input type="checkbox" onChange={(e) => handleQuery(e, "Category")} id={item.toLowerCase()} value={(item == "All") ? "" : item} /> <label className="text-[15px] font-[lora]">{item}</label>
         </div>
     ))
     const filtersByPrice = filter.Price.map((item, index) => (
         <div className="" key={index}>
-            <input type="checkbox" onChange={handleInput} id={item.toLowerCase()} value={item} /> <label className="text-[15px] font-[lora]">{item} <span>GHS</span></label>
+            <input type="checkbox" onChange={(e) => handleQuery(e, "Price")} id={item.toLowerCase()} value={item} /> <label className="text-[15px] font-[lora]">{item} <span>GHS</span></label>
         </div>
     ))
     const filterByColor = filter.Color.map((item, index) => (
         <div className="" key={index}>
-            <input type="checkbox" onChange={handleInput} id={item.toLowerCase()} value={item} /> <label className="text-[15px] ">{item}</label>
+            <input type="checkbox" onChange={(e) => handleQuery(e, "Color")} id={item.toLowerCase()} value={item == "All" ? "" : item} /> <label className="text-[15px] ">{item}</label>
         </div>
     ))
+
     return (
         <>
             <header>
                 <Navbar />
             </header>
-                <main className="grid grid-cols-[1fr,4fr] gap-5 ">
-                    <aside className="border-r-2">
-                        <div className="ac-Filters p-5">
-                            <div className="ac-Category" >
-                                <h2 className="text-[1.1rem] mb-5 font-bold uppercase font-[lora]">Category</h2>
-                                {filtersByCategory}
-                            </div>
-                            <div className="ac-Price">
-                                <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Price </h2>
-                                {filtersByPrice}
-                            </div>
-                            <div className="ac-Color">
-                                <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Color</h2>
-                                {filterByColor}
-                            </div>
+            <main className="grid grid-cols-[1fr,4fr] ">
+                <aside className="border-r-2">
+                    <div className="ac-Filters p-5">
+                        <div className="ac-Category" >
+                            <h2 className="text-[1.1rem] mb-5 font-bold uppercase font-[lora]">Category</h2>
+                            {filtersByCategory}
                         </div>
-                    </aside>
-                    <section>
+                        <div className="ac-Price">
+                            <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Price </h2>
+                            {filtersByPrice}
+                        </div>
+                        <div className="ac-Color">
+                            <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Color</h2>
+                            {filterByColor}
+                        </div>
+                    </div>
+                </aside>
+                <section className="px-5 pt-10 h-[90vh] overflow-y-scroll">
+                    <div className="ac_Items flex flex-wrap justify-center gap-3 py-5 ">
+                        {
+                            products.map( (P, index) => (
+                                <div key={index} className="item w-[180px] min-h-[220px] my-3 text-left  border-2">
+                                    <img src={`http://localhost:5330/public/img/${P.Image}`} alt={P.Image} className="w-full h-[100px]" />
+                                    <div className="ac_textItem p-3">
+                                        <p className="text-[16px] font-[Merriweather] font-bold">{P.Nom}</p>
+                                        <p className="text-[12px]">{P.Description}</p>
+                                        <p className="text-[18px] font-[Open-Sans]"> <span className="text-[16px] font-[Merriweather] mr-2 font-bold">Price:</span>{P.Price} GHS</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
 
-                    </section>
-                </main>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
