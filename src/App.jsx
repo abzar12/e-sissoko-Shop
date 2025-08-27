fc4e14d8b4c5a6021a61e7c4881e309095ee086f
AbouCloud <Abouzari6839@gmail.com>
2025-08-27 22:02:30 +0000

    edit page has an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AddProduct from "./AddProduct"
 import Home from "./Home"
+import EditProduct from "./EditProduct"
 import { Routes, Route } from "react-router-dom"
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<AddProduct />} />
+        <Route  path="/product/edit-product" element={<EditProduct />} />
       </Routes>
     </>
 
