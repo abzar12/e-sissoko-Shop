d7b2c98d238f846bd1824142fd4909b6fdf7fa20
AbouCloud <Abouzari6839@gmail.com>
2025-09-02 23:29:39 +0000

    beginner of handle filter of server side

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,10 +20,11 @@ function Home() {
         })
     }
     // fetching the product on the table
+    // filters data to the server
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const resp = await fetch(`http://localhost:5330/product/edit-product`);
+                const resp = await fetch(`http://localhost:5330/product/show-product?query=${query}`);
                 if (!resp.ok) {
                     throw new Error(`Please Check : ${resp.status}`)
                 }
@@ -35,7 +36,7 @@ function Home() {
         }
         fetchData();
     }, [query])
-
+    // handle filters 
     const filtersByCategory = filter.Category.map((item, index) => (
         <div className="" key={index}>
             <input type="checkbox" onChange={(e) => handleQuery(e, "Category")} id={item.toLowerCase()} value={(item == "All") ? "" : item} /> <label className="text-[15px] font-[lora]">{item}</label>
@@ -78,15 +79,15 @@ function Home() {
                     <div className="ac_Items flex flex-wrap justify-center items-end gap-3 py-5 ">
                         {
                             products.map((P, index) => (
-                                <div key={index} className="item w-[180px] min-h-[220px] my-5 text-left mb-0 border-2  ">
-                                   <Link to={`/product/product-detail?${P.Id_phone}`}> <img src={`http://localhost:5330/public/img/${P.Image}`} alt={P.Image} className="w-full h-[100px] object-contain bg-center" /> </Link>
+                                <div key={index} className="item w-[180px] min-h-[220px] my-5 text-left mb-0 border-2 overflow-hidden ">
+                                    <Link to={`/product/product-detail?${P.Id_phone}`}> <img src={`http://localhost:5330/public/img/${P.Image}`} alt={P.Image} className="w-full h-[100px] object-contain bg-center hover:scale-110 transition-all duration-300 z-0" /> </Link>
                                     <div className="ac_textItem px-3 h-auto pt-3">
                                         <p className="text-[16px] font-[Merriweather] font-bold">{P.Nom}</p>
                                         <p className="text-[12px]">{P.Description}</p>
                                         <p className="text-[18px] font-[Open-Sans]"> <span className="text-[16px] font-[Merriweather] mr-2 font-bold">Price:</span>{P.Price} GHS</p>
                                     </div>
                                     <div className="Add_btn mx-3 mt-2">
-                                    <Link to={auth ? `/product/edit-product?Id=${P.Id_phone}` : `/product/product-detail?${P.Id_phone}`} > <button type="button" className="border-gray-500 hover:border-green-700 font-[Merriweather] border rounded-[50px] relative py-1 w-full mb-2 transition-all duration-[0.2s] ease-in hover:bg-green-600 hover:text-white"> {auth ? "Edit Product" : "Add to Cart"}</button> </Link>
+                                        <Link to={auth ? `/product/edit-product?Id=${P.Id_phone}` : `/product/product-detail?${P.Id_phone}`} > <button type="button" className="border-gray-500 hover:border-green-700 font-[Merriweather] border rounded-[50px] relative py-1 w-full mb-2 transition-all duration-[0.2s] ease-in hover:bg-green-600 hover:text-white"> {auth ? "Edit Product" : "Add to Cart"}</button> </Link>
                                     </div>
                                 </div>
                             ))
