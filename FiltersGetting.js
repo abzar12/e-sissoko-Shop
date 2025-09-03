6ee44dcae5df6066d057cdf0e907d4c9f56346c8
AbouCloud <Abouzari6839@gmail.com>
2025-09-03 22:13:10 +0000

    filter price has been completed it left categories and color

--- FiltersGetting.js
+++ FiltersGetting.js
@@ -1,5 +1,5 @@
 export default {
         Category: ["All", "Phone", "Shoe", "Watche", "Computer", "Headphone"],
-        Price : [ "0 - 1000", "1000 - 2000", "2000 - 5000", "min 5000"],
+        Price : [ "0 - 1000", "1000 - 2000", "2000 - 5000", "greater than 5000"],
         Color : ["All", "Black", "White", "Red", "Brown", "Gray", "Yellow" ]
     }
