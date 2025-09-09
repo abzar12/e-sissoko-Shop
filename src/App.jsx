884d7de9a2e2b3b464fded272da7eca722cc9022
AbouCloud <Abouzari6839@gmail.com>
2025-09-09 19:32:41 +0000

    login page created but still working on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AddProduct from "./AddProduct"
 import Home from "./Home"
 import EditProduct from "./EditProduct"
+import Login from "./Log-in"
 import { Routes, Route } from "react-router-dom"
 export default function App() {
   return (
@@ -8,7 +9,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<AddProduct />} />
-        <Route  path="/product/edit-product" element={<EditProduct />} />
+        <Route path="/product/edit-product" element={<EditProduct />} />
+        <Route path="/e-sissoko/log-in/" element={<Login />} />
       </Routes>
     </>
 
