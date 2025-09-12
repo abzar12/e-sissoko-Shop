89e244c0f378ce26e9432f259bd767d4bc553cac
AbouCloud <Abouzari6839@gmail.com>
2025-09-12 18:46:34 +0000

    login page still works on it

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 export default function Navbar() {
-   
+
     return (
         <>
             <div className="ac-navbar bg-[#2B2B2B] text-white max-w-[95%] rounded-xl mx-auto">
@@ -12,21 +12,23 @@ export default function Navbar() {
                     <label htmlFor="">LOGO</label>
                 </div>
                 <div className="ac_search">
-                    <input type="text" placeholder="Search..." id="search-items" className="  bg-transparent" />
+                    <input type="text" placeholder="Search..." id="search-items" className="outline-none  bg-transparent" />
                     <div className="search-icon">
                         <FaSearch />
                     </div>
                 </div>
                 <div className="side_btn">
-                    <div className="btn-icon"><button type="button bg-transparent "><FaRegHeart className="text-[20px]"/></button></div>
-                    <div className="btn-icon relative group">
-                         <Link to="/e-sissoko/log-in" className="" ><button type="button bg-transparent"><FaUser className="text-[20px]"/></button></Link> 
-                         <span className="absolute top-full opacity-0 cursor-default group-hover:opacity-100 transition px-2 py-1 left-[-20px] text-sm rounded-lg bg-gray-300 text-black w-16">Log-In</span>
+                    <div className="btn-icon"><button type="button bg-transparent "><FaRegHeart className="text-[20px]" /></button></div>
+                    <div className="btn-icon relative group ">
+                        <Link to="/e-sissoko/log-in" className="" ><button type=" button hover:text-white/50 bg-transparent "><FaUser className="text-[20px] " /></button></Link>
+                        <span className="absolute top-full opacity-0 cursor-default group-hover:opacity-100 transition px-2 py-1 left-[-20px] text-sm rounded-lg bg-gray-300 text-black w-[59px]">Log-In</span>
                     </div>
-                    
-                    <div className="btn-icon"><button type="button bg-transparent"><FaShoppingCart className="text-[20px]"/></button></div>
+                    <div className="group relative">
+                        <div className="btn-icon"><button type="button bg-transparent"><FaShoppingCart className="text-[20px]" /></button></div>
+                        <span className="absolute top-full opacity-0 cursor-default group-hover:opacity-100 transition px-2 py-1 left-[-10px] text-sm rounded-lg bg-gray-300 text-black ">Card</span>
+                    </div>
                 </div>
-                
+
             </div>
         </>
     )
