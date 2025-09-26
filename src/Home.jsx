763b86806eacd062f1eb554f6a16ba574e2f324e
AbouCloud <Abouzari6839@gmail.com>
2025-09-26 19:28:48 +0000

    new design and section hero added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./component/Navbar"
+import Hero from "./component/hero"
 import filter from '../FiltersGetting'
-import './style/home.css'
+import "./component/style/home.css"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 function Home() {
@@ -56,40 +57,47 @@ function Home() {
 
     return (
         <>
-            <header>
-                <Navbar />
+            <header >
+                <Hero />
             </header>
+
             <main className="grid grid-cols-[1fr,4fr] ">
-                <aside className=" ">
-                    <div className="ac-Filters p-5 relative">
+                <aside className="ac-filter">
+                    <div className="container">
                         <div className="ac-Category" >
-                            <h2 className="text-[1.1rem] mb-5 font-bold uppercase font-[lora]">Category</h2>
+                            <h2 className="">Category</h2>
                             {filtersByCategory}
                         </div>
                         <div className="ac-Price">
-                            <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Price</h2>
+                            <h2 className="">Price</h2>
                             {filtersByPrice}
                         </div>
                         <div className="ac-Color">
-                            <h2 className="text-[1.1rem] my-3 font-bold uppercase font-[lora]">Color</h2>
+                            <h2 className="">Color</h2>
                             {filterByColor}
                         </div>
                     </div>
                 </aside>
                 {/* card items */}
-                <section className="px-5 pt-10 h-[90vh] overflow-y-scroll">
-                    <div className="ac_Items flex flex-wrap justify-center items-end gap-3 py-5 ">
+                <section className="section-card ">
+                    <div className="ac_Items">
                         {
                             products.map((P, index) => (
-                                <div key={index} className="item bg-white rounded-lg w-[180px] min-h-[220px] my-5 text-left mb-0 border-2 overflow-hidden ">
-                                    <Link to={`/product/product-detail?${P.Id_phone}`}> <img src={`http://localhost:5330/public/img/${P.Image}`} alt={P.Image} className="w-full h-[100px] object-contain bg-center hover:scale-110 transition-all duration-300 z-0" /> </Link>
-                                    <div className="ac_textItem px-3 h-auto pt-3">
-                                        <p className="text-[16px] font-[Merriweather] font-bold">{P.Nom}</p>
-                                        <p className="text-[12px]">{P.Description}</p>
-                                        <p className="text-[18px] font-[Open-Sans]"> <span className="text-[16px] font-[Merriweather] mr-2 font-bold">Price:</span>{P.Price} GHS</p>
+                                <div key={index} className="items">
+                                    <div className="card-image">
+                                        <Link to={`/product/product-detail?${P.Id_phone}`}>
+                                            <img src={`http://localhost:5330/public/img/${P.Image}`} alt={P.Image} />
+                                        </Link>
                                     </div>
-                                    <div className="Add_btn mx-3 mt-2">
-                                        <Link to={auth ? `/product/edit-product?Id=${P.Id_phone}` : `/product/product-detail?${P.Id_phone}`} > <button type="button" className="border-gray-500 hover:bg-[#2B2B2B] font-[Merriweather] border rounded-[50px] relative py-1 w-full mb-2 transition-all duration-[0.2s] ease-in hover:text-white "> {auth ? "Edit Product" : "Add to Cart"}</button> </Link>
+                                    <div className="ac_textItem">
+                                        <p className="title">{P.Nom}</p>
+                                        <p className="desc">{P.Description}</p>
+                                        <p className="price"><span> Price:</span>{P.Price_Promo ? P.Price_Promo : P.Price} GHS  <span className="special">{P.Price_Promo ? P.Price : ""}</span> </p>
+                                    </div>
+                                    <div className="card_btn">
+                                        <Link to={auth ? `/product/edit-product?Id=${P.Id_phone}` : `/product/product-detail?${P.Id_phone}`} >
+                                            <button type="button"> {auth ? "Edit Product" : "Add to Cart"}</button>
+                                        </Link>
                                     </div>
                                 </div>
                             ))
