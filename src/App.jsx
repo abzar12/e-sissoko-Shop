dd07e90523aac81bc9032ae0125a85446311cbe5
AbouCloud <Abouzari6839@gmail.com>
2025-12-14 08:43:15 +0000

    nav bar and product detail and many other page has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,27 @@ import AddProduct from "./pages/AddProduct"
 import Home from "./pages/Home"
 import EditProduct from "./pages/EditProduct"
 import Login from "./pages/Log-in"
-import ShopDetail from "./pages/shop_detail"
+import ShopDetail from "./pages/shopDetails/shop_detail"
 import Test from "./pages/test"
-import Select from "./component/Select/select"
-import Reackform from "./component/formhooks/reackform"
-import MyAlert from "./component/PopUp/myAlert"
-
 import { useTheme } from "./pages/themeContext"
 import { Routes, Route } from "react-router-dom"
+import Cart from "./pages/Cart"
+import CartProvider from "./component/Context/cartContext/cartContext"
+
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop/add-product" element={<AddProduct />} />
-        <Route path="/product/edit-product" element={<EditProduct />} />
-        <Route path="/e-sissoko/log-in/" element={<Login />} />
-        <Route path="/shop/product-detail" element={<ShopDetail />} />
-        <Route path="/shop/product" element={<Test />} />
-        <Route path="/select" element={<Select />} />
-        <Route path="/ReactForm" element={<Reackform />} />
-        <Route path="/myAlert" element={<MyAlert />} />
-      </Routes>
+      <CartProvider >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop/add-product" element={<AddProduct />} />
+          <Route path="/product/edit-product" element={<EditProduct />} />
+          <Route path="/e-sissoko/log-in/" element={<Login />} />
+          <Route path="/shop/product-detail" element={<ShopDetail />} />
+          <Route path="/shop/product" element={<Test />} />
+          <Route path="/shop/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </>
   )
 }
