5faee6534dfd222a2077bd87006b54a7dc13e256
AbouCloud <Abouzari6839@gmail.com>
2025-12-21 07:58:52 +0000

    updated Navbar and loading component  about the design it way mess up

--- src/component/dashboard/navbar.jsx
+++ src/component/dashboard/navbar.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBurger } from "react-icons/fa6";
-function Navbar(){
+import style from "../../component/style/dashboard/dasNavbar.module.css"
+import { BiMenu, BiSearch, BiBell } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
+function Navbar({ onButtonClick }) {
     return (
         <>
-        <div className="ac-container">
-            <div className="ac-navbar">
-                <ul>
-                <button type="button"><FaBurger /></button>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
+            <div className={style.ac_container}>
+                <div className={style.shortNavbar}>
+                    <div className={style.content}>
+                        <div className={style.ac_btn_Box}>
+                            <BiMenu className={style.ac_iconMenu} onClick={onButtonClick}/>
+                        </div>
+                        <div>
+                            <Link to="/" className={style.link}>Home</Link>
+                        </div>
+                    </div>
+                    <div className={style.content}>
+                        <form action="" className={style.searchBox}>
+                            <input type="text" placeholder="Search..." />
+                            <button type="submit"> <BiSearch className={style.ac_iconSearch} /> </button>
+                        </form>
+                        <div className={style.ac_btn_Box}>
+                            <button type="button"> <BiBell className={style.ac_iconBell} /> <span className={style.notification}>0</span></button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="">
-                
-            </div>
-        </div>
         </>
     )
 }
--- src/component/loading/BtnLoading.jsx
+++ src/component/loading/BtnLoading.jsx
@@ -2,8 +2,8 @@ import "./Loading.css"
 export default function BtnLoading() {
     return (
         <>
-            <div className="flex justify-center items-center content-center max-h-full">
-                <div className="dot-spinner">
+            <div className="flex justify-center items-center content-center  max-h-full">
+                <div className="dot-spinner ">
                     <div className="dot-spinner__dot"></div>
                     <div className="dot-spinner__dot"></div>
                     <div className="dot-spinner__dot"></div>
