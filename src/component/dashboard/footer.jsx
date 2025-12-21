ebb4515f169ae2b43c0ec3044e2cbc4cf15bb21a
AbouCloud <Abouzari6839@gmail.com>
2025-12-21 08:08:00 +0000

    dashboard updated with his features

---
+++ src/component/dashboard/footer.jsx
@@ -0,0 +1,12 @@
+import style from "../../component/style/dashboard/dasFooter.module.css"
+
+function Footer(){
+    return(
+        <>
+        <div className={style.container}>
+            <h1>this a footer</h1>
+        </div>
+        </>
+    )
+}
+export default Footer
