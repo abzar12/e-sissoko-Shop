26deafc58700371fe82cf1ac9d8beeaf7c690bf8
AbouCloud <Abouzari6839@gmail.com>
2025-12-21 08:06:06 +0000

    this is sideBar in dashboard contains the text and icon of side

---
+++ sideBarDarshboard.js
@@ -0,0 +1,15 @@
+import { MdDashboard, MdAddShoppingCart } from "react-icons/md"
+import { FaProductHunt, FaPeopleLine } from "react-icons/fa6";
+import { FaShippingFast, FaMoneyCheckAlt } from "react-icons/fa";
+
+
+
+export  const  SideBarIconText = [
+    {icon: MdDashboard, text:"Dashboard"},
+    {icon: FaProductHunt, text:"Products"},
+    {icon: MdAddShoppingCart, text:"Orders"},
+    {icon: FaPeopleLine, text:"Customers"},
+    {icon: FaMoneyCheckAlt, text:"Payment"},
+    {icon: FaShippingFast, text:"Shipping"}
+    
+]
