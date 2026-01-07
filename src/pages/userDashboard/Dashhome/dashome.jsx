52f7a7b2ad702f0f408526fa46bf7f9eaf3c8621
AbouCloud <Abouzari6839@gmail.com>
2026-01-07 15:46:27 +0000

    updated check out and it left the card transaction

---
+++ src/pages/userDashboard/Dashhome/dashome.jsx
@@ -0,0 +1,9 @@
+
+function DashHome() {
+    return (
+        <>
+            <h1>this is a dashboard Home</h1>
+        </>
+    )
+}
+export default DashHome
---
+++ src/pages/userDashboard/dashboard.jsx
@@ -0,0 +1,31 @@
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../../component/Context/authContext/authContext";
+import Aside from "../../component/dashboard/Aside";
+import style from "../../component/style/dashboard/dashboard.module.css"
+import Content from "../../component/dashboard/content";
+function Dashboard() {
+    const { user } = useAuth()
+    
+    const [isopen, setIsopen] = useState(true)
+    const handleClick = () =>{
+        setIsopen(!isopen)
+    }
+    console.log("Dashboard rendered ") 
+    if(user.role !== "staff" || user.role !== "director"){
+        return <Navigate to="/" replace />
+    }
+    return (
+        <>
+            <div className={style.container}>
+                <aside>
+                    <Aside isopen={isopen} />
+                </aside>
+                <main className={`${style.content} ${isopen ? style.activeBtn + " transition-all duration-300 ease-in" : null} `}>
+                    <Content onButtonClick={handleClick} />
+                </main>
+            </div>
+        </>
+    )
+}
+export default Dashboard;
