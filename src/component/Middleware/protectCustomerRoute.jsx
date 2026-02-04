c4b32c0d3051f8aadf1d7811cd79737a6c2e5ea7
AbouCloud <Abouzari6839@gmail.com>
2026-02-04 21:40:13 +0000

    protect customer route page added

---
+++ src/component/Middleware/protectCustomerRoute.jsx
@@ -0,0 +1,27 @@
+import { useAuth } from "../Context/authContext/authContext";
+import { Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Loading from "../loading/Loading";
+function ProtectCustomerRoute({ Customer }) {
+    const { accessToken, isloading, user } = useAuth();
+    if (isloading) {
+        return (
+            <> 
+                <div className="">
+                    <Loading />
+                </div>
+            </>
+        )
+    }
+    if (accessToken && user.role === "customer") {
+        return <Outlet />
+    }
+    // if (!accessToken && !Customer) {
+    //     return <Navigate to="/e-sissoko/log-in/" replace />
+    // }
+    // if(user.role !== "customer"){
+        return <Navigate to="/login-me/" replace />
+    // }
+}
+
+export default ProtectCustomerRoute
