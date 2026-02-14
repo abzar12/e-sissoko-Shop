1f36c1f7033c0e2ec0c02ec02f19900532225e11
AbouCloud <Abouzari6839@gmail.com>
2026-02-14 22:07:44 +0000

    working on the responsibility and theme added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import ThemeProvider from './pages/themeContext.jsx'
 import "./component/style/theme.css"
 import "./index.css"
 import App from './App.jsx'
 import CartProvider from './component/Context/cartContext/cartContext.jsx'
-import {AuthProvider} from './component/Context/authContext/authContext.jsx'
+import { AuthProvider } from './component/Context/authContext/authContext.jsx'
+import { ThemProvider } from './component/Context/themContext/them.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider >
       <CartProvider>
-        <App />
+        <ThemProvider >
+          <App />
+        </ThemProvider>
       </CartProvider>
     </AuthProvider>
 
