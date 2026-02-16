1e67086960d6d9734fce163bca0637ad0f0efdbb
AbouCloud <Abouzari6839@gmail.com>
2026-02-16 21:09:36 +0000

    forgot password created

---
+++ src/pages/password/customerPassword.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+import MyAlert from "../../component/PopUp/myAlert"
+import { useNavigate } from "react-router-dom"
+export function CustomerPassword() {
+    const navigate = useNavigate()
+    const [status, setStatus] = useState(false)
+    const [show, setShow] = useState(false)
+    const [children, setChildren] = useState(null)
+    const [Email, setEmail] = useState(null)
+
+    const handleEmail = async (e) => {
+        e.preventDefault()
+        const formdata = new FormData(e.target)
+        const inputemail = formdata.get('email')
+        try {
+            const resp = await fetch(`${import.meta.env.VITE_API_URL}/customers/forgotPassword/${inputemail}`,{
+                method: "POST",
+                headers:{"Content-Type": "application/json"}
+            })
+            const data = await resp.json()
+            if (!resp.ok || !data.success) {
+                setShow(true)
+                setChildren(data.message)
+                throw new Error(`Request Failed Status:${resp.status}`);
+            }
+            setShow(true)
+            setStatus(true)
+            setChildren(data.message)
+            setEmail(data.email)
+        } catch (error) {
+            throw new Error(`Error: ${error.message}`)
+        }
+    }
+    const handleClose = () => {
+        setShow(false)
+        if(status) navigate(`/customer/verify-OTP?email=${Email}`)
+    }
+    return (
+        <>
+            <MyAlert onClose={handleClose} show={show} status={status} children={children} />
+            <div className="w-full">
+                <div className="sm:mx-auto mx-3 mt-[35vh]  bg-[var(--bg-color-primary)] min-w-[100px] max-w-[400px] shadow-[1px_1px_15px_var(--card-shadow)] p-3 text-center rounded-md text-[var(--text-color-secondary)]">
+                    <h1>Forget Password</h1>
+                    <p className="opacity-50 mt-3" >Please type your email to get and OTP</p>
+                    <form action="" onSubmit={handleEmail} className="px-5">
+                        <div className="w-full border px-3 py-1 sm:py-2 rounded-md my-3">
+                            <input type="email" placeholder="example@gmail.com"
+                                className=" outline-none focus:outline-none bg-transparent w-full " name="email" required />
+                        </div>
+                        <div className=" hover:text-[var(--text-color)] border-2 border-[var(--bg-color)] py-1 rounded-full mt-5 hover:bg-[var(--bg-color)] transition-all duration-300 ease-in">
+                            <button type="submit" className="w-full">Send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
