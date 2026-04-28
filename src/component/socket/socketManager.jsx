54305ff842652e3e5eb27bb75f32e8097258c09c
AbouCloud <Abouzari6839@gmail.com>
2026-04-28 07:13:48 +0000

    socket for real updating disable

--- src/component/socket/socketManager.jsx
+++ src/component/socket/socketManager.jsx
@@ -1,43 +1,43 @@
-import { useEffect } from "react";
-import { socket } from "./socket";
-import { useAuth } from "../Context/authContext/authContext";
+// import { useEffect } from "react";
+// import { socket } from "./socket";
+// import { useAuth } from "../Context/authContext/authContext";
 
-function SocketManager() {
-  const { user } = useAuth();
+// function SocketManager() {
+//   const { user } = useAuth();
 
-  // Join user room
-  useEffect(() => {
-    if (!user?.id) return;
+//   // Join user room
+//   useEffect(() => {
+//     if (!user?.id) return;
 
-    const onConnect = () => {
-      socket.emit("joinUser", user.id);
-    };
+//     const onConnect = () => {
+//       socket.emit("joinUser", user.id);
+//     };
 
-    socket.on("connect", onConnect);
+//     socket.on("connect", onConnect);
 
-    return () => {
-      socket.emit("leaveUser", user.id);
-      socket.off("connect", onConnect);
-    };
-  }, [user?.id]);
+//     return () => {
+//       socket.emit("leaveUser", user.id);
+//       socket.off("connect", onConnect);
+//     };
+//   }, [user?.id]);
 
-  // Join admin room
-  useEffect(() => {
-    const onConnect = () => {
-      socket.emit("joinAdmin");
-    };
+//   // Join admin room
+//   useEffect(() => {
+//     const onConnect = () => {
+//       socket.emit("joinAdmin");
+//     };
 
-    socket.on("connect", onConnect);
-    socket.on("connect", () => {
-      console.log("✅ socket connected", socket.id);
-    });
-    return () => {
-      socket.emit("leaveAdmin");
-      socket.off("connect", onConnect);
-    };
-  }, []);
+//     socket.on("connect", onConnect);
+//     socket.on("connect", () => {
+//       console.log("✅ socket connected", socket.id);
+//     });
+//     return () => {
+//       socket.emit("leaveAdmin");
+//       socket.off("connect", onConnect);
+//     };
+//   }, []);
 
-  return null; // no UI needed
-}
+//   return null; // no UI needed
+// }
 
-export default SocketManager;
+// export default SocketManager;
