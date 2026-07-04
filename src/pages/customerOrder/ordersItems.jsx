4e0edc7dc5afbed63fcd49fad686f0d9276b46df
AbouCloud <Abouzari6839@gmail.com>
2026-07-04 16:08:02 +0000

    itemsView fixed and updated

--- src/pages/customerOrder/ordersItems.jsx
+++ src/pages/customerOrder/ordersItems.jsx
@@ -4,47 +4,57 @@ import { useOutletContext } from "react-router-dom"
 import Button from "../../component/Button/button"
 
 function OrdersItems() {
-    const {Data} = useOutletContext()
+    const { Data } = useOutletContext()
     const navigate = useNavigate()
 
     const orders = Data?.orders || []
-     const formatDate = (d) => {
+    const formatDate = (d) => {
         try {
             return new Date(d).toLocaleString()
         } catch (e) {
             return d
         }
     }
+    console.log("data, ", Data)
     return (
         <>
             {
                 orders.length > 0 ?
                     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
                         {
-
-                            orders.map((item, index) => (
-                                <div key={index || item.Order_Number} className="bg-[var(--bg-primary)] sm:flex gap-3 flex-wrap items-center shadow-md rounded-md">
-                                    <div className=" flex flex-wrap">
-                                        <img src={`${item?.Images[0]?.url ? item.Images[0].url : import.meta.env.VITE_API_URL+"/public/upload/Product_img/"+item.Images[0]}`} className="h-28 w-28 rounded-md" />
-                                        <div className="overflow-hidden ">
-                                            <h3 className="text-lg line-clamp-1 font-semibold text-gray-500 truncate">{item.Name}</h3>
-                                            <p className="text-sm text-gray-500">Order: <span className="font-medium">{item.Order_Number}</span></p>
-                                            <p className="text-sm text-gray-500">Qty: <span className="font-medium">{item.Quantity}</span></p>
-                                            <p className="text-sm text-gray-500">Price: <span className="font-medium">{item.Price}</span></p>
-                                            <p className="text-sm text-gray-500">Delivery: <span className="font-medium">{item.Delivery_Method || '—'}</span></p>
-                                            <p className="text-xs text-gray-400 mt-1">Placed: {formatDate(item.Create_At)}</p>
-                                        </div>
-                                    </div>
-                                    <div className=" py-2 flex-shrink-0 flex-1 flex flex-wrap sm:flex-nowrap justify-center items-center gap-3 border rounded-md px-1">
-                                        <p className="text-sm text-gray-500 inline-flex">Items( <span className="font-medium">{item.Item_Number}</span>)</p>
-                                        <span className="text-black font-bold text-sm sm:text-lg">Amount: {item.Total_Amount} GHS</span>
-                                        <span className={`px-3 py-1 rounded-full text-sm ${item.Ordered_Status === 'delivered' ? 'bg-green-600' : item.Ordered_Status === 'Shipped' ? 'bg-blue-600' : 'bg-yellow-600'}`}>{item.Ordered_Status}</span>
-                                        <div className="">
-                                            <Button children="View" onClick={() => navigate(`/orders/view/?ID=${item._id || item.Order_Number}`)} className="px-3  rounded-full" />
-                                        </div>
-                                    </div>
+                            orders.map((item, index) => {
+                               let Images = [];
+                                try {
+                                    Images = typeof item.Images === "string"
+                                        ? JSON.parse(item.Images)
+                                        : item.Images || [];
+                                } catch (err) {
+                                    Images = [];
+                                }
+                        return (
+                        <div key={index || item.Order_Number} className="bg-[var(--bg-primary)] sm:flex gap-3 flex-wrap items-center shadow-md rounded-md">
+                            <div className=" flex flex-wrap px-3">
+                                {/* <img src={`${Images[0]?.url ? Images[0]?.url : import.meta.env.VITE_API_URL + "/public/upload/Product_img/" + Images[0]}`} alt={Images[0]} className="h-28 w-28 rounded-md" /> */}
+                                <div className="overflow-hidden ">
+                                    <h3 className="text-lg line-clamp-1 font-semibold text-gray-500 truncate">{item.Name}</h3>
+                                    <p className="text-sm text-gray-500">Order: <span className="font-medium">{item.Order_Number}</span></p>
+                                    {/* <p className="text-sm text-gray-500">Qty: <span className="font-medium">{item.Quantity}</span></p> */}
+                                    {/* <p className="text-sm text-gray-500">Price: <span className="font-medium">{item.Price}</span></p> */}
+                                    <p className="text-sm text-gray-500">Delivery: <span className="font-medium">{item.Delivery_Method || '—'}</span></p>
+                                    <p className="text-xs text-gray-400 mt-1">Placed: {formatDate(item.Created_At)}</p>
                                 </div>
-                            ))
+                            </div>
+                            <div className=" py-2 flex-shrink-0 flex-1 flex flex-wrap sm:flex-nowrap justify-center items-center gap-3 border rounded-md px-1">
+                                <p className="text-sm text-gray-500 inline-flex">Items( <span className="font-medium">{item.Item_Number}</span>)</p>
+                                <span className="text-black font-bold text-sm sm:text-lg">Amount: {item.Total_Amount} GHS</span>
+                                <span className={`px-3 py-1 rounded-full text-sm ${item.Ordered_Status === 'delivered' ? 'bg-green-600' : item.Ordered_Status === 'Shipped' ? 'bg-blue-600' : 'bg-yellow-600'}`}>{item.Ordered_Status}</span>
+                                <div className="">
+                                    <Button children="View" onClick={() => navigate(`/orders/view/?ID=${item._id || item.Order_Number}`)} className="px-3  rounded-full" />
+                                </div>
+                            </div>
+                        </div>
+                        );
+                            })
                         }
                     </div>
                     :
